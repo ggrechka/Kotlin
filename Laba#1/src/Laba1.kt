04727d56ae83501935dee1c8bbf49f4f5d398d2e
Grechka <kurchenko-e@mail.ru>
2021-04-25 23:18:58 +0300

    #3

--- Laba#1/src/Laba1.kt
+++ Laba#1/src/Laba1.kt
@@ -1,2 +1,15 @@
 //1 задание
-fun main()=println("Hello World!")
+//fun main()=println("Hello World!")
+
+//3 задание
+    fun main() {
+        if (args.size == 0) {
+            print("Введите ваше имя:")
+            return
+        }
+        println("Привет, ${args[0]}!")
+        println("Привет, %s!".format(args[0]))
+        println("Привет " + args[0]+ "!")
+        println("Привет,\n${args[0]}\n!")
+
+    }
