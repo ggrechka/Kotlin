5bc554e9286718f60bc116f128d22c6ebd809f42
Grechka <kurchenko-e@mail.ru>
2021-04-25 23:27:21 +0300

    #5

--- Laba#1/src/Laba1.kt
+++ Laba#1/src/Laba1.kt
@@ -15,15 +15,34 @@
 //    }
 
 //4 задание
-fun main() {
-    var stroka = readLine()
-    if (stroka != null) {
-        println("Метод length: ${stroka.length}")
-        println("Метод hashCode: ${stroka.hashCode()}")
-        println("Метод capitalize: ${stroka.capitalize()}")
-        println("Метод chunked: ${stroka.chunked(3)}")
-        println("Метод drop: ${stroka.drop(3)}")
-        println("Метод isEmpty: ${stroka.isEmpty()}")
-        println("Метод plus: ${stroka.plus("ssss")}")
+//fun main() {
+//    var stroka = readLine()
+//    if (stroka != null) {
+//        println("Метод length: ${stroka.length}")
+//        println("Метод hashCode: ${stroka.hashCode()}")
+//        println("Метод capitalize: ${stroka.capitalize()}")
+//        println("Метод chunked: ${stroka.chunked(3)}")
+//        println("Метод drop: ${stroka.drop(3)}")
+//        println("Метод isEmpty: ${stroka.isEmpty()}")
+//        println("Метод plus: ${stroka.plus("ssss")}")
+//    }
+//}
+
+//5 задание
+fun main(args: Array<String>) {
+    if (args.size == 0) {
+        print("Введите ваше имя:")
+        return
+    }
+    println("Привет, ${args[0]}!")
+    println("Какой у тебя любимый язык(го на английском)?")
+    var language = readLine()
+    when (language) {
+        "Prolog", "Kotlin" -> println("Ну ты и подлиза...")
+        "C#" -> println("Я был о тебе лучшего мнения")
+        "Python" -> println("Уф, красавчик!")
+        "C++" -> println("Спасибо, что не Си.....")
+        "Pascal" -> println("Гаркуши на тебя нет...")
+        else -> println("Таких языков на ФКТиПМ не знают(грустняшка).")
     }
 }
