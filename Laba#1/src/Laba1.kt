1ab9afad23e751d40803d0a2f3528a9365d64632
Grechka <kurchenko-e@mail.ru>
2021-04-25 23:21:17 +0300

    #4

--- Laba#1/src/Laba1.kt
+++ Laba#1/src/Laba1.kt
@@ -2,14 +2,28 @@
 //fun main()=println("Hello World!")
 
 //3 задание
-    fun main() {
-        if (args.size == 0) {
-            print("Введите ваше имя:")
-            return
-        }
-        println("Привет, ${args[0]}!")
-        println("Привет, %s!".format(args[0]))
-        println("Привет " + args[0]+ "!")
-        println("Привет,\n${args[0]}\n!")
+//    fun main() {
+//        if (args.size == 0) {
+//            print("Введите ваше имя:")
+//            return
+//        }
+//        println("Привет, ${args[0]}!")
+//        println("Привет, %s!".format(args[0]))
+//        println("Привет " + args[0]+ "!")
+//        println("Привет,\n${args[0]}\n!")
+//
+//    }
 
+//4 задание
+fun main() {
+    var stroka = readLine()
+    if (stroka != null) {
+        println("Метод length: ${stroka.length}")
+        println("Метод hashCode: ${stroka.hashCode()}")
+        println("Метод capitalize: ${stroka.capitalize()}")
+        println("Метод chunked: ${stroka.chunked(3)}")
+        println("Метод drop: ${stroka.drop(3)}")
+        println("Метод isEmpty: ${stroka.isEmpty()}")
+        println("Метод plus: ${stroka.plus("ssss")}")
     }
+}
