5b0a631119ffbac853ac71602ad43dd1857d8a10
Grechka <kurchenko-e@mail.ru>
2021-04-26 01:52:48 +0300

    #6

--- Laba#1/src/Laba1.kt
+++ Laba#1/src/Laba1.kt
@@ -1,3 +1,6 @@
+import java.util.*
+import kotlin.math.min
+
 //1 задание
 //fun main()=println("Hello World!")
 
@@ -29,20 +32,49 @@
 //}
 
 //5 задание
-fun main(args: Array<String>) {
-    if (args.size == 0) {
-        print("Введите ваше имя:")
-        return
+//fun main(args: Array<String>) {
+//    if (args.size == 0) {
+//        print("Введите ваше имя:")
+//        return
+//    }
+//    println("Привет, ${args[0]}!")
+//    println("Какой у тебя любимый язык(го на английском)?")
+//    var language = readLine()
+//    when (language) {
+//        "Prolog", "Kotlin" -> println("Ну ты и подлиза...")
+//        "C#" -> println("Я был о тебе лучшего мнения")
+//        "Python" -> println("Уф, красавчик!")
+//        "C++" -> println("Спасибо, что не Си.....")
+//        "Pascal" -> println("Гаркуши на тебя нет...")
+//        else -> println("Таких языков на ФКТиПМ не знают(грустняшка).")
+//    }
+//}
+
+//6 задание
+
+fun main() {
+    print("Введите число: ")
+    //1 вариант ввода - спросить
+    //var a= readLine()
+    //var s= a?.toInt()
+
+    //2 вариант ввода
+    val input = Scanner(System.`in`)
+    var a = input.nextInt()
+    var aCopy = a
+    var sum = 0
+    while (a != 0) {
+        sum += a % 10
+        a = a/10
     }
-    println("Привет, ${args[0]}!")
-    println("Какой у тебя любимый язык(го на английском)?")
-    var language = readLine()
-    when (language) {
-        "Prolog", "Kotlin" -> println("Ну ты и подлиза...")
-        "C#" -> println("Я был о тебе лучшего мнения")
-        "Python" -> println("Уф, красавчик!")
-        "C++" -> println("Спасибо, что не Си.....")
-        "Pascal" -> println("Гаркуши на тебя нет...")
-        else -> println("Таких языков на ФКТиПМ не знают(грустняшка).")
-    }
+    println("Сумма цифр числа $sum")
+
+    //Методы класса Int
+    println("Метод and: ${aCopy.and(10)}") // конъюнкция
+    println("Метод compareTo: ${aCopy.compareTo(11)}")
+    println("Метод dec: ${aCopy.dec()}")
+    println("Метод div: ${aCopy.div(2.5)}")
+    println("Метод minus:${aCopy.minus(3)}")
+    println("Метод rangeTo: ${aCopy.rangeTo(20)}")
+    println("Метод xor:${aCopy.xor(4)}")
 }
