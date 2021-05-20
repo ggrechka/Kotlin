fb5a93e5d45c7eec3fbc8a2ce4135221ae76e08a
Grechka <kurchenko-e@mail.ru>
2021-05-20 13:14:48 +0300

    #2

--- Laba#2/src/Laba2.kt
+++ Laba#2/src/Laba2.kt
@@ -2,12 +2,29 @@ import java.util.*
 
 // 1 задание
 
-fun sumDigit(num: Int):Long = if(num!=0) {
-    val sum = sumDigit(num / 10)
-    sum + (num % 10)
-} else 0
+//fun sumDigit(num: Int):Long = if(num!=0) {
+//    val sum = sumDigit(num / 10)
+//    sum + (num % 10)
+//} else 0
+//
+//fun main() {
+//    val input = Scanner(System.`in`)
+//    val a = input.nextInt()
+//    println("Сумма цифр: ${sumDigit(a)}")
+//}
+
+// 2 задание
+
+fun sumDigit(num: Int, sum: Int = 0): Int = if (num != 0) {
+    sumDigit(num / 10,sum + (num % 10))
+} else sum
+
+tailrec fun sumDigit(num: Int, sum: Int = 0): Int = if (num != 0) {
+    sumDigit(num / 10,sum + (num % 10))
+} else sum
 
 fun main() {
+    print("Введите число: ")
     val input = Scanner(System.`in`)
     val a = input.nextInt()
     println("Сумма цифр: ${sumDigit(a)}")
