8e3b89158404ca7127c761512ba84d9c71f77e20
Grechka <kurchenko-e@mail.ru>
2021-05-20 17:31:50 +0300

    #5

--- Laba#2/src/Laba2.kt
+++ Laba#2/src/Laba2.kt
@@ -92,42 +92,115 @@ import java.util.*
 
 // 4 задание
 
-fun sumDigit(num: Int): Int = if (num != 0) {
-    val sum = sumDigit(num / 10)
-    sum + (num % 10)
-} else 0
+//fun sumDigit(num: Int): Int = if (num != 0) {
+//    val sum = sumDigit(num / 10)
+//    sum + (num % 10)
+//} else 0
+//
+//fun proizDigit(num: Int): Int = if (num != 0) {
+//    val proiz = proizDigit(num / 10)
+//    proiz * (num % 10)
+//} else 1
+//
+//fun minDigit(num: Int): Int {
+//    val newNum = num / 10
+//    val digit = num % 10
+//
+//    return if (newNum != 0) {
+//        val min = minDigit(newNum)
+//        if (digit < min)
+//            digit
+//        else min
+//    } else digit
+//}
+//
+//fun maxDigit(num: Int): Int {
+//    val newNum = num / 10
+//    val digit = num % 10
+//
+//    return if (newNum != 0) {
+//        val max = maxDigit(newNum)
+//        if (digit > max)
+//            digit
+//        else max
+//    } else digit
+//}
+//
+//fun searchDigit(num: Int, func: (number: Int) -> Int, inVal: Int = 0): Int =
+//    func(num)
+//
+//fun main() {
+//    print("Введите число: ")
+//    val input = Scanner(System.`in`)
+//    val a = input.nextInt()
+//
+//    println(
+//        "Выберите команду: \n" +
+//                "1.Поиск суммы цифр числа \n" +
+//                "2.Поиск минимальной цифры \n" +
+//                "3.Поиск максимальной цифры \n" +
+//                "4.Поиск произведения цифр числа"
+//    )
+//    when (input.nextInt()) {
+//        1 -> println("Сумма цифр: ${searchDigit(a, ::sumDigit)}")
+//        2 -> println("Минимальная цифра: ${searchDigit(a, ::minDigit)}")
+//        3 -> println("Максимальная цифра: ${searchDigit(a, ::maxDigit)}")
+//        4 -> println("Произведение цифр: ${searchDigit(a, ::proizDigit)}")
+//    }
+//}
 
-fun proizDigit(num: Int): Int = if (num != 0) {
-    val proiz = proizDigit(num / 10)
-    proiz * (num % 10)
-} else 1
+//5 задание
 
-fun minDigit(num: Int): Int {
-    val newNum = num / 10
-    val digit = num % 10
+tailrec fun sumDigit(num: Int, inVal: Int = 0): Int = if (num != 0) {
+    sumDigit(num / 10, inVal + (num % 10))
+} else inVal
 
-    return if (newNum != 0) {
-        val min = minDigit(newNum)
-        if (digit < min)
-            digit
-        else min
-    } else digit
-}
+tailrec fun minDigit(num: Int, inVal: Int = num % 10): Int = if (num != 0) {
+    minDigit(num / 10, if (num % 10 < inVal) num % 10 else inVal)
+} else inVal
 
-fun maxDigit(num: Int): Int {
-    val newNum = num / 10
-    val digit = num % 10
+tailrec fun maxDigit(num: Int, inVal: Int = num % 10): Int = if (num != 0) {
+    maxDigit(num / 10, if (num % 10 > inVal) num % 10 else inVal)
+} else inVal
 
-    return if (newNum != 0) {
-        val max = maxDigit(newNum)
-        if (digit > max)
-            digit
-        else max
-    } else digit
-}
+tailrec fun proizDigit(num: Int, inVal: Int = 1): Int = if (num != 0) {
+    proizDigit(num / 10, inVal * (num % 10))
+} else inVal
 
-fun searchDigit(num: Int, func: (number: Int) -> Int, inVal: Int = 0): Int =
-    func(num)
+
+
+fun searchDigit(num: Int, func1: (number: Int) -> Boolean, func2: (number: Int) -> Int, inVal: Int = 0): Int =
+    if (func1(num)) {
+        func2(num)
+    } else throw Exception()
+
+fun condition1(num: Int): Boolean =
+    if (num != 0) {
+        if (((num % 10) % 2) == 0) {
+            condition1(num / 10)
+        } else false
+    } else true
+
+fun condition2(num: Int): Boolean =
+    if (num != 0) {
+        if (((num % 10) % 2) == 1) {
+            condition2(num / 10)
+        } else false
+    } else true
+
+fun condition3(num: Int): Boolean =
+    if (num != 0) {
+        if (((num % 10) % 3) == 0) {
+            condition3(num / 10)
+        } else false
+    } else true
+
+fun checkCondition(check:String,num :Int):Boolean=
+    if (check=="1") {
+        condition1(num)
+    } else if (check=="2"){
+        condition2(num)
+    } else condition3(num)
 
 fun main() {
     print("Введите число: ")
@@ -141,10 +214,22 @@ fun main() {
                 "3.Поиск максимальной цифры \n" +
                 "4.Поиск произведения цифр числа"
     )
-    when (input.nextInt()) {
-        1 -> println("Сумма цифр: ${searchDigit(a, ::sumDigit)}")
-        2 -> println("Минимальная цифра: ${searchDigit(a, ::minDigit)}")
-        3 -> println("Максимальная цифра: ${searchDigit(a, ::maxDigit)}")
-        4 -> println("Произведение цифр: ${searchDigit(a, ::proizDigit)}")
+    val command=input.nextInt()
+    println(
+        "Выберите условие фильтрации: \n" +
+                "1.Все цифры числа чётные \n" +
+                "2.Все цифры числа нечётные \n" +
+                "3.Все цифры числа кратны 3"
+    )
+    val condition=input.nextInt()
+    try {
+        when (command) {
+            1 -> println("Сумма цифр: ${searchDigit(a,  if (condition==1) ::condition1 else if (condition==2) ::condition2 else ::condition3, ::sumDigit)}")
+            2 -> println("Минимальная цифра: ${searchDigit(a,  if (condition==1) ::condition1 else if (condition==2) ::condition2 else ::condition3, ::minDigit)}")
+            3 -> println("Максимальная цифра: ${searchDigit(a,  if (condition==1) ::condition1 else if (condition==2) ::condition2 else ::condition3, ::maxDigit)}")
+            4 -> println("Произведение цифр: ${searchDigit(a,  if (condition==1) ::condition1 else if (condition==2) ::condition2 else ::condition3, ::proizDigit)}")
+        }
+    } catch (e: Exception) {
+        println("Условие не пройдено")
     }
 }
