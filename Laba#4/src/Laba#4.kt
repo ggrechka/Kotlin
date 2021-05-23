5a126ba6bff6b270f65a115a556a20458ed74795
Grechka <kurchenko-e@mail.ru>
2021-05-23 15:25:32 +0300

    #2.5

--- Laba#4/src/Laba#4.kt
+++ Laba#4/src/Laba#4.kt
@@ -20,7 +20,29 @@ tailrec fun obxodString(s: String, inVal: Int = -100000): Int =
 
     } else inVal
 
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*   для второго задания  -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
+//fun printMass(mas: CharArray, i: Int = 0): String =
+//    if (i != mas.size) {
+//        println(mas[i])
+//        printMass(mas, i + 1)
+//    } else "\n"
+
+fun printMass2(mas: IntArray, i: Int = 0): String =
+    if (i != mas.size) {
+        println(mas[i])
+        printMass2(mas, i + 1)
+    } else "\n"
+
+//5
+fun randomCharInString(s: String): String {
+    val listS = s.toList()
+    val newS = listS.shuffled()
+    var res = newS.toString()
+    res = res.removeSurrounding("[", "]")
+    res = res.replace(",", "")
+    return res
+}
 
 
 
@@ -31,7 +53,13 @@ fun main() {
     println("Введите строку: ")
 // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*   для первого задания  -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
-    val stroka = "234 3434 76453 3223 1 4 9 3 0"
-    println(obxodString(stroka))
+//    val stroka = "234 3434 76453 3223 1 4 9 3 0"
+//    println(obxodString(stroka))
+
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*   для второго задания  -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+//    5
+    val stroka = "a 7 b 8 155 3 5 130"
+    println(randomCharInString(stroka))
+//
 
 }
