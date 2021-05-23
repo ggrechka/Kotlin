16909fd05413f25efe239218ecd11a0ac44211d0
Grechka <kurchenko-e@mail.ru>
2021-05-23 15:41:43 +0300

    #2.7

--- Laba#4/src/Laba#4.kt
+++ Laba#4/src/Laba#4.kt
@@ -44,13 +44,29 @@ fun randomCharInString(s: String): String {
     return res
 }
 
+//7
+tailrec fun createArrayFromString(s: String, inVal: CharArray = CharArray(0), i: Int = 0): CharArray =
+    if (i < s.length) {
+        if (s[i] in 'a'..'z') {
+            val temp = inVal + s[i]
+            createArrayFromString(s, temp, i + 1)
+        } else createArrayFromString(s, inVal, i + 1)
 
+    } else inVal
+
+fun Palindrom(s: CharArray, i: Int = 0): String =
+    if (i < (s.size) / 2) {
+        if (s[i] != s[s.size - i - 1]) {
+            "не палиндром"
+        } else Palindrom(s, i + 1)
+
+    } else "палиндром"
 
 
 fun main() {
 
     val input = Scanner(System.`in`)
-    println("Введите строку: ")
+//    println("Введите строку: ")
 // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*   для первого задания  -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 //    val stroka = "234 3434 76453 3223 1 4 9 3 0"
@@ -58,8 +74,16 @@ fun main() {
 
 // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*   для второго задания  -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 //    5
-    val stroka = "a 7 b 8 155 3 5 130"
-    println(randomCharInString(stroka))
-//
+//    val stroka = "a 7 b 8 155 3 5 130"
+//    println(randomCharInString(stroka))
+
+//    7
+    val stroka1 = "a b Z d e x g e E" //пример для отриц.ответа
+    val stroka2 = "a B c d e d c Z a" //пример для полож.ответа с неч.кол-вом символов
+    val stroka3 = "a B c d e e d c Z a" //пример для полож.ответа с чётным кол-вом символов
+    val stroka4 = "a B e d e d e a" //пример для полож.ответа c интересным случаем
+
+    val mas = createArrayFromString(stroka4)
+    println("Ваша строка ${Palindrom(mas)}")
 
 }
